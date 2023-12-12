e9c0b1296714319431095fd1c5c0cefc4cd31350
SazukeR <reinaldojcg1@gmail.com>
2023-12-12 10:26:28 -0500

    Add ng-template and ngIfElse

--- src/app/heroes/list/list.component.ts
+++ src/app/heroes/list/list.component.ts
@@ -11,8 +11,10 @@ export class ListComponent {
 
   public heroNames: string[] = ["Spiderman", "Hulk", "Ironman", "Thor", "Wonder woman"]
 
+  public deletedHero?: string // creamos la propiedad aqui para que pueda ser usada en el template
 
 
-
-
+  deleteLastHero(): void {
+    this.deletedHero = this.heroNames.pop() // pop ademas de eliminar el utimo elemento de un arreglo, tambien retorna dicho elemento, asi que lo que haremos es guardar ese elemento eliminado en una variable publica dentro de la clase para mostrar el nombre del elemento eliminado
+  }
 }
